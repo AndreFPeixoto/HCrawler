84e34bc3e9ccd5fe69e4f38305cc5c111ba72611
André Peixoto <andrefilipe.1810@gmail.com>
2023-02-17 11:53:46 +0000

    initial commit

---
+++ crawler/main.py
@@ -0,0 +1,13 @@
+import pyfiglet
+
+
+def display_header():
+    print(pyfiglet.figlet_format("HCrawler", "slant"))
+
+
+def main():
+    display_header()
+
+
+if __name__ == "__main__":
+    main()
